7c0f75af21228e0727a14f9484ca6ff92066a7a1
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-23 19:59:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,6 +52,6 @@ end
 def get_average_age_for_season(data, season)
   arr = []
   data[season].each do |hash|
-      return hash["age"].map {|num|}.reduce(:+) 
+      return hash["age"].map {|num| num}.reduce(:+) 
   end
 end
